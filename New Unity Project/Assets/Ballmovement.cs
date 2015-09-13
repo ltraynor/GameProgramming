62a39331353a43d0f8299bd6461f7956afb19508
Sadip Giri <sadipgiri@bennington.edu>
2015-09-13 19:59:06 -0400

    plz work

---
+++ New Unity Project/Assets/Ballmovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ballmovement : MonoBehaviour {
+ 
+	//make ourselves a rigid body
+	Rigidbody physicsBody; 
+
+	void Start () {
+
+		physicsBody = GetComponent<Rigidbody>(); 
+	}
+	
+	// FixedUpdate is called once before physics calculations
+	void FixedUpdate () {
+		//this is so we can move
+		float zMovement = Input.GetAxis ("Vertical"); 
+		float xMovement = Input.GetAxis ("Horizontal");
+
+		//multiply input by this number and that is how fast the sphere moves
+		int Speed = 90;
+
+		float scaledXMovement = xMovement * Speed;
+		float scaledZMovement = zMovement * Speed;
+
+		//actually move the thing
+		physicsBody.velocity = new Vector3(scaledXMovement,0,scaledZMovement); 
+
+	
+	}
+
+	//this is for collisions
+	void OnTriggerEnter(Collider token) { 
+		token.gameObject.SetActive(false);
+
+	}
+}
