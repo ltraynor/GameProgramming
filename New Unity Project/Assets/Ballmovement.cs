c92a3e701c93ff3990702d59f07a18c607c13672
Sadip Giri <sadipgiri@bennington.edu>
2015-09-17 21:46:56 -0400

    okay checking in the correct version now, the one which has the lose condition
    If you hit the wall, you lose :)

--- New Unity Project/Assets/Ballmovement.cs
+++ New Unity Project/Assets/Ballmovement.cs
@@ -10,18 +10,30 @@ public class Ballmovement : MonoBehaviour {
 	//multiply input by this number and that is how fast the sphere moves
 	public int Speed = 90;
 	private int rawScore;	//the score
-	public Text displayedScore;
-	public Text winText;
+	//these are the text variables
+	public Text displayedScore; //score
+	public Text winText;	//winning display
+	public Text loseText;	//losing display (not added yet)
+	//have you won yet?
+	bool wonYet;
+	bool lostYet; //have you lost yet?
 
 	void Start () {
+		//score starts at 0
 		rawScore = 0;
-		//void OnGUI() {
-		//	GUI.Label(new Rect(0, 0, 100, 25), "Score: " + Score);
-		//}
+		//give our ball a body
 		physicsBody = GetComponent<Rigidbody>();
+		//set the scoreboard (see below function)
 		SetScoreBoard ();
-		winText.text = "You Win!!!!";
+		//if you happen to win...
+		winText.text = "You Win!!!!"; //remains unactive until then
 		winText.gameObject.SetActive (false);
+		//same deal with losing...
+		loseText.text = "You lose... :(";
+		loseText.gameObject.SetActive (false);
+		//you haven't won yet
+		wonYet = false;
+		lostYet = false;
 	}
 	
 	// FixedUpdate is called once before physics calculations
@@ -37,8 +49,14 @@ public class Ballmovement : MonoBehaviour {
 		float scaledZMovement = zMovement * Speed;
 
 		//actually move the thing
-		physicsBody.velocity = new Vector3(scaledXMovement,0,scaledZMovement); 
+		physicsBody.velocity = new Vector3(scaledXMovement,0,scaledZMovement);
 
+		Vector3 playerPos = gameObject.transform.position;
+
+		if ((playerPos.x > 45 || playerPos.x < -45 || playerPos.z > 45 || playerPos.z < -45) && wonYet != true)  {
+			loseText.gameObject.SetActive(true);
+			lostYet = true;
+		}
 	
 	}
 
@@ -46,12 +64,18 @@ public class Ballmovement : MonoBehaviour {
 	//this is for collisions
 	void OnTriggerEnter(Collider token) {
 		//Destroy (token.gameObject);
+		//increase your score
 		rawScore++;
+		//set the scoreboard again
 		SetScoreBoard ();
+		//object becomes false
 		token.gameObject.SetActive(false);
-		if (rawScore == 4) {
-			winText.gameObject.SetActive(true);
+		//if you have enough points to win...!!
+		if (rawScore == 4 && lostYet != true) {
+			winText.gameObject.SetActive(true);	//then you win
+			wonYet = true;
 		}
+
 
 	}
 
